715524331e814a0bf2b35fc555f1c19694b2f0e3
78526Nasir <islam35-1102@diu.edu.bd>
2018-04-07 22:13:51 +0600

    Raw handwritten script added

---
+++ A2R/Script/script.js
@@ -0,0 +1,15 @@
+﻿
+function addTextBox() {
+    var btnSpanDiv = document.getElementById("btnSpanDiv");
+    var hiddenTextBoxs = document.getElementById("ht");
+    hiddenTextBoxs.style.display = "flex";
+    btnSpanDiv.style.display = "none";
+}
+
+function hideMessage() {
+    var label = document.getElementById("ContentPlaceHolder1_lblMessage");
+    label.style.display = "none";
+    var h1 = document.getElementById("ContentPlaceHolder1_h1NotFound");
+    h1.style.display = "none";
+    
+}
